0ecb5737fcd2de2789e2c2f256100e41b90cb502
trident-kanda <tridentkandayuki@gmail.com>
2021-06-07 19:20:38 +0900

    typescript

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <h1 class=" text-4xl">aaaa</h1>
+  <Layout>
+    <div class="bg-white p-6">
+      <img src="./assets/logo.png" alt="vue" class=" text-center m-auto" width="150" height="150">
+      <h1 class=" text-center text-green-500 text-5xl ">Vue TODO LIST</h1>
+      <Title name="LIST"/>
+      <Title name="ADD"/>
+    </div>
+  </Layout>
 </template>
 
-<script>
-
-
-export default {
-  name: 'App',
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Layout from "./components/Layout.vue"
+import Title from "./components/Title.vue"
+@Options({
   components: {
-    
-  }
-}
+    Layout,
+    Title
+  },
+})
+export default class App extends Vue {}
 </script>
 
 
